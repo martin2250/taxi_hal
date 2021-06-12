739617ee7a9442e2fdd169e56a8d6efc94147555
Martin Pittermann <martinp2250@gmail.com>
2021-06-12 09:17:13 +0200

    add fanout board

--- src/taxi_hal/udaq.py
+++ src/taxi_hal/udaq.py
@@ -69,17 +69,43 @@ class UDaq:
             self.set_adc_config(*adc)
         self.send_line('SET_LIVETIME_ENABLE 1')
 
-
-class TaxiUdaqInterface:
-    def __init__(self):
-        # port 0: pwr_en, port 1: pwr_fault
-        self.ic6 = PCA9555A(self.i2c_write, 0x40)
-        # port 0: [prog_en : 1, prog_addr : 3], port 1: [pwr_mon : 2]
-        self.ic9 = PCA9555A(self.i2c_write, 0x42)
-
+class Taxi:
     def get_buildinfo(self) -> str:
         year = smc.read_16(8)
         month = smc.read_16(10)
         day = smc.read_16(12)
         version = smc.read_16(14)
         return f'{year:04X}-{month:02X}-{day:02X} v{version:04X}'
+
+class TaxiFanoutBoard:
+    def __init__(self):
+        # port 0: pwr_en, port 1: pwr_fault
+        self.ic6 = PCA9555A(0x40)
+        # port 0: [prog_en : 1, prog_addr : 3], port 1: [pwr_mon : 2]
+        self.ic9 = PCA9555A(0x42)
+    
+    def setup(self):
+        self.ic6.set_direction(PCA9555A.PORT_0, 0x00) # pwr en
+        self.ic6.set_direction(PCA9555A.PORT_1, 0xff) # pwr fault
+        self.ic9.set_direction(PCA9555A.PORT_0, 0xf0) # prog addr, en
+        self.ic9.set_direction(PCA9555A.PORT_1, 0xff) # pwr mon (24V redundant inputs)
+
+    def set_udaq_prog(self, enable: bool, port: int = 0):
+        if enable:
+            # set ~prog_en low to enable programming
+            self.ic9.set_output(PCA9555A.PORT_0, port & 0b0111)
+        else:
+            # set ~prog_en high to disable programming
+            self.ic9.set_output(PCA9555A.PORT_0, 0b1000)
+
+    def get_power_mon(self) -> int:
+        return self.ic9.get_input(PCA9555A.PORT_1) >> 6
+
+    def get_udaq_fault(self) -> int:
+        return self.ic6.get_input(PCA9555A.PORT_1)
+    
+    def set_udaq_power(self, mask: int):
+        self.ic6.set_output(PCA9555A.PORT_0, mask & 0xff)
+    
+    def get_udaq_power(self) -> int:
+        return self.ic6.get_output(PCA9555A.PORT_0)
