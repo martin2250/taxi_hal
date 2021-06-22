9ea8f3050aa5cf9b795bc89964b5751fc7ec3433
Martin Pittermann <martinp2250@gmail.com>
2021-06-22 11:54:17 +0200

    fix udaq channel selector

--- src/taxi_hal/commands/udaq_prog.py
+++ src/taxi_hal/commands/udaq_prog.py
@@ -16,7 +16,7 @@ def udaq_channel(desc: str) -> list[str]:
         if '-' in part:
             start, _, stop = part.partition('-')
             for i in range(int(start), int(stop) + 1):
-                if i not in range(7):
+                if i not in range(8):
                     raise ValueError(f'channel {i} is out of range')
                 channels.add(i)
         else:
